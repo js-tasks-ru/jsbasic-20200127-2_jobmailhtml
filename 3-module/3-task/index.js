8ca7a3746bae8bdb3f6419574c226825097c0784
verstka <jobmailhtml@gmail.com>
2020-02-03 20:34:43 +0200

    3 module 2 and 3 task - decision

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -3,4 +3,14 @@
  * @returns {string}
  */
 function camelize(str) {
+  let array = str.split('-');
+  let newArr = array.slice(0, 1);
+
+  for (let item of array.slice(1)) {
+    newArr.push(item[0].toUpperCase() + item.slice(1));
+  }
+
+  str = newArr.join("");
+
+  return str;
 }
