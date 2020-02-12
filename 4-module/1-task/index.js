36be892b2fd00b8d3777e26cd6cd805eb8d22310
verstka <jobmailhtml@gmail.com>
2020-02-12 21:02:12 +0200

    4 module 1 task - decision

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -4,4 +4,14 @@
  * @return {HTMLUListElement}
  */
 function makeFriendsList(friends) {
+  let node = document.createElement("ul");
+  let fragment = new DocumentFragment();
+
+  for (let variable of friends) {
+    let li = document.createElement('li');
+    li.append(variable.firstName + " " + variable.lastName);
+    fragment.append(li);
+  }
+
+  return node.append(fragment);
 }
